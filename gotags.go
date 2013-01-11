20f41209d54ccff6a041ad921ce9cb6a1dd04ce4
Xian Xu <xx@twitter.com>
2013-01-10 22:09:26 -0800

    remove outdated comments

--- gotags.go
+++ gotags.go
@@ -1,16 +1,3 @@
-/*
-This is a program to generate ctags for go. I'm using this with vim and ctrl-p.
-As a result, it's not particularly fully implemented, just the portion of stuff I need.
-
-The basic functionality provided is to construct a tags file under current running directory.
-Several src directory can be passed in, all in the relative form of current directory. All go
-source files in those directories will be recursively parsed and root level declarations
-extracted. Currently private declarations are extracted as well, for easier reading of source
-code.
-
-So to use, do:
-	gotags dir1 dir2 file1
- */
 package main
 
 import (
