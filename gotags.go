450a656f550311f849f8f6b171c682cc25f853ce
Xian Xu <xx@twitter.com>
2013-01-10 21:48:25 -0800

    update based on my useage.

--- gotags.go
+++ gotags.go
@@ -10,40 +10,54 @@ code.
 
 So to use, do:
 	gotags dir1 dir2 file1
-
-For golang dev though, I think the golang plugin for IntelliJ's very good. With IdeaVim plugin,
-you don't really need this vim+ctags setting. My 2c.
  */
 package main
 
 import (
-	"fmt"
-    "flag"
-	"os"
 	"bufio"
-	"strings"
+	"bytes"
+	"fmt"
 	"go/ast"
 	"go/parser"
 	"go/printer"
 	"go/token"
-	"bytes"
+	"os"
 	"path/filepath"
+	"strings"
+    "flag"
 )
 
 const (
 	sep = "."
-	header = `!_TAG_FILE_FORMAT	2	/extended format; --format=1 will not append ;" to lines/
+	header =
+`!_TAG_FILE_FORMAT	2	/extended format; --format=1 will not append ;" to lines/
 !_TAG_FILE_SORTED	0	/0=unsorted, 1=sorted, 2=foldcase/
 !_TAG_PROGRAM_AUTHOR	Xian Xu	/email/
 !_TAG_PROGRAM_NAME	gotags	//
 !_TAG_PROGRAM_URL	http://github.com/xianxu/gotags	/official site/
 !_TAG_PROGRAM_VERSION	1.0	//
 `
+	helpText =
+`This program generates ctags' tags file, for the intention to be used in vim and ctrl-p. To use,
+simply pass in a list of directories and go source files. Directories are processed recursively.
+
+Examples:
+
+  gotags .
+  gotags src ../go/src/pkg
+
+Tag generate contain some additional information. E.g. os.func.Exit would be one tag. Typically
+I use gotags src/ ../go/src/pkg to generate tags that contains both src files of my project and
+source files of sdk. `
 )
 
 func main() {
     flag.Parse()
 	paths := flag.Args()
+	if flag.NArg() == 0 {
+		printUsage()
+		os.Exit(0)
+	}
 
 	// a channel of tags discovered.
 	tags := make(chan string, 10)
@@ -82,11 +96,15 @@ func main() {
 	<-done
 }
 
+func printUsage() {
+	fmt.Println(helpText)
+}
+
 func consume(tags chan string, done chan int) {
 	tf, err := os.Create("tags")
 	if err != nil {
-		fmt.Printf("%v", err)
-		panic("Can't open tags file for write.")
+		fmt.Println("Can't open tags file for write.")
+		os.Exit(1)
 	}
 	writer := bufio.NewWriter(tf)
 	writer.WriteString(header)
@@ -122,14 +140,14 @@ func funcToString(fset *token.FileSet, n interface{}) string {
 	}
 	f := n.(*ast.FuncDecl)
 	var buf bytes.Buffer
-	if f.Recv == nil {
-		buf.WriteString("func" + sep)
-	} else {
-		buf.WriteString("method" + sep + fieldsToString(fset, f.Recv) + sep)
-	}
 	buf.WriteString(f.Name.Name)
-	buf.WriteString("("+fieldsToString(fset, f.Type.Params)+")")
-	buf.WriteString("("+fieldsToString(fset, f.Type.Results)+")")
+	if f.Recv == nil {
+		buf.WriteString(sep + "func")
+	} else {
+		buf.WriteString(sep + "func" + sep + fieldsToString(fset, f.Recv))
+	}
+	//buf.WriteString("("+fieldsToString(fset, f.Type.Params)+")")
+	//buf.WriteString("("+fieldsToString(fset, f.Type.Results)+")")
 	return buf.String()
 }
 func fieldsToString(fset *token.FileSet, fl *ast.FieldList) string {
@@ -175,25 +193,21 @@ func parseGo(prefix string, fset *token.FileSet, tree *ast.File, tags chan strin
 						continue
 					}
 					if _, ok := m.Type.(*ast.StructType); ok {
-						tags <- prefix + "struct" + sep + m.Name.Name + "\t" + nodeToLoc(fset, m)
+						tags <- prefix + m.Name.Name + sep + "type" + "\t" + nodeToLoc(fset, m)
 					} else if _, ok := m.Type.(*ast.InterfaceType); ok {
-						tags <- prefix + "interface" + sep + m.Name.Name + "\t" + nodeToLoc(fset, m)
-					} else if id, ok := m.Type.(*ast.Ident); ok {
-						tags <- prefix + "struct" + sep + m.Name.Name + " " + id.Name + "\t" + nodeToLoc(fset, m)
+						tags <- prefix + m.Name.Name + sep + "type" + "\t" + nodeToLoc(fset, m)
+					} else if _, ok := m.Type.(*ast.Ident); ok {
+						tags <- prefix + m.Name.Name + sep + "type" + "\t" + nodeToLoc(fset, m)
 					}
 				case *ast.ValueSpec:
 					for _, v := range m.Names {
 						if !isExported(v.Name) {
 							continue
 						}
-						ts := nodeToString(fset, m.Type)
-						if len(ts) > 0 {
-							ts = " " + ts
-						}
 						if n.Tok == token.CONST {
-							tags <- prefix + "const" + sep + v.Name + ts + "\t" + nodeToLoc(fset, v)
+							tags <- prefix + v.Name + sep + "const" + "\t" + nodeToLoc(fset, v)
 						} else if n.Tok == token.VAR {
-							tags <- prefix + "var" + sep + v.Name + ts + "\t" + nodeToLoc(fset, v)
+							tags <- prefix + v.Name + sep + "var" + "\t" + nodeToLoc(fset, v)
 						}
 					}
 				}
